a2e73fe5acb9c66cb56bec95017305ef5d415a0d
iryanclarke <clarke_93@hotmail.com>
2016-01-26 14:08:42 -0500

    Fixing the namespace listening

--- routes/sockets.js
+++ routes/sockets.js
@@ -13,27 +13,30 @@
  module.exports.listen = function(app){
      io = socketio.listen(app)
 
+     // Stats room
      stats = io.of('/stats');
      stats.on('connection', function(socket){
          stats.emit('Score', { hello: 'My Kajigger' });
      })
 
-
+     // Mobilia room
      mobilia = io.of('/mobilia');
      mobilia.on('connection', function(socket){
-        mobilia.emit('news', { hello: 'DACEHPTYV' });
-        mobilia.on('beep', function(){
-          mobilia.emit('boop');
-        });
+        //socket.join('mobilia');
+        mobilia.emit('news', { hello: 'Mobilia' });
+
+        socket.on('beep', function(){
+         mobilia.emit('boop');
+       });
      })
 
 
-
+     // Unity 'room'
      io.on('connection', function(socket){
-        socket.emit('news', { hello: 'DACEHPTYV' });
+        socket.emit('news', { hello: 'Unity' });
        	socket.on('beep', function(){
        		socket.emit('boop');
-          mobilia.emit('boop');
+          mobilia.emit('unity');
        	});
         io.on('open', function(){
             io.emit('success', { hello: 'World' });
